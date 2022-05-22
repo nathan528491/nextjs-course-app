aaea19ea7bdef31b2a48722da4d83d9449803e9f
Nathan <nathan.lancman@edu.ece.fr>
2022-05-22 21:24:33 +0200

    Fixing ERR_HTTP_HEADERS_SENT issue

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -3,26 +3,24 @@ import { jsx } from 'theme-ui'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-const Note = () => {
+export default function Note({note}) {
   const router = useRouter()
   const { id }= router.query
 
   return (
     <div sx={{variant: 'containers.page'}}>
-      <h1>Note: {id} </h1>
+      <h1>Note: {note.title} </h1>
     </div>
   )
 }
 
-export default Note
+
 
 export async function getServerSideProps({params, req, res}) {
   const response = await fetch(`http://localhost:3000/api/note/${params.id}`)
 
   if (!response.ok) {
-    res.writeHead(302, { Location: '/notes' })
-    res.end()
-    return {props: {}}
+    return res.writeHead(302, { Location: "/notes" }).end()
   }
 
   const {data} = await response.json()
